27fc674032c7605700ea599ee7e64d1256ee09a2
Nanna <naho0001@stud.kea.dk>
2024-04-03 11:12:32 +0200

    jhiu

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+
+    theme: {
+        extend: {
+          rotate: {
+            '140': '140deg',
+          }
+        }
+      },
+
+    
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     plugins: [
         require('@tailwindcss/typography'),
